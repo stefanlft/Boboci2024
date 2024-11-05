55376f80b09c40a4e8c18d4b1429a994ff4190b3
srb <74992183+minusonee@users.noreply.github.com>
2024-11-05 14:30:13 +0200

    fisiere pentru tema 2

---
+++ Crane.java
@@ -0,0 +1,128 @@
+package org.firstinspires.ftc.teamcode.drive.subsystems;
+
+import com.arcrobotics.ftclib.hardware.ServoEx;
+import com.qualcomm.robotcore.hardware.AnalogInput;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcontroller.external.samples.SensorTouch;
+
+public class Crane {
+    //MOTOR DECLARATION
+    public DcMotor motorCrane1, motorCrane2;
+    public CRServo servoGrippy, servoSlide1, servoSlide2;
+    public TouchSensor slideSensor;
+    public AnalogInput slideEncoder;
+    //PID VARIABLES
+    public int craneTarget = 0;
+    public static double craneP = 0.017, craneI = 0, craneD = 0.0005;
+    double craneIntegralSum = 0;
+    public static double craneF = 0.28;
+    public static int craneOffset = 90;
+    public double slideExtension = 0;
+    public double slideEncoderLastPosition = 0;
+    private final double threetwelvemotorticksindegree = 537.7 / 360;
+    double craneLastError = 0;
+    public int gripperDirection = 1;
+    public int slidesDirection = 1;
+    public double gripperPower = 1;
+    public double slidesPower = 1;
+    ElapsedTime craneTimer = new ElapsedTime();
+
+    //MOTOR INIT
+    public Crane(HardwareMap hardwareMap){
+        motorCrane1 = hardwareMap.dcMotor.get("motorCrane1");
+        motorCrane2 = hardwareMap.dcMotor.get("motorCrane2");
+
+        servoGrippy = hardwareMap.crservo.get("servoGrippy");
+        servoSlide1 = hardwareMap.crservo.get("servoSlide1");
+        servoSlide2 = hardwareMap.crservo.get("servoSlide2");
+
+        slideSensor = hardwareMap.touchSensor.get("slideSensor");
+
+        slideEncoder = hardwareMap.analogInput.get("slideEncoder");
+
+        motorCrane1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorCrane1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorCrane1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorCrane1.setDirection(DcMotorSimple.Direction.FORWARD);
+        motorCrane2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorCrane2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorCrane2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorCrane2.setDirection(DcMotorSimple.Direction.REVERSE);
+        servoGrippy.setDirection(CRServo.Direction.FORWARD);
+        servoSlide1.setDirection(CRServo.Direction.FORWARD);
+        servoSlide2.setDirection(CRServo.Direction.REVERSE);
+
+    }
+    //MOVE THE SLIDES FUNCTION
+    public void setSlides(double slidesPower){
+        if(slidesDirection == 1){
+            servoSlide1.setDirection(DcMotorSimple.Direction.FORWARD);
+            servoSlide2.setDirection(DcMotorSimple.Direction.REVERSE);
+            if(slideExtension < 120 && slideSensor.isPressed()){
+                servoSlide1.setPower(0);
+                servoSlide2.setPower(0);
+            }
+            else {
+                servoSlide1.setPower(slidesPower);
+                servoSlide2.setPower(slidesPower);
+            }
+        }
+
+        else if (slidesDirection == -1){
+            servoSlide1.setDirection(DcMotorSimple.Direction.REVERSE);
+            servoSlide2.setDirection(DcMotorSimple.Direction.FORWARD);
+            if(slideExtension > 150 && slideSensor.isPressed()){
+                servoSlide1.setPower(0);
+                servoSlide2.setPower(0);
+            }
+            else {
+                servoSlide1.setPower(slidesPower);
+                servoSlide2.setPower(slidesPower);
+            }
+        }
+
+    }
+
+    //SET DIRECTION AND THE POWER OF THE GRIPPER
+    public void setGripper(double gripperPower){
+        if(gripperDirection == 1) {
+            servoGrippy.setDirection(DcMotorSimple.Direction.FORWARD);
+        }
+        else if(gripperDirection == -1) {
+            servoGrippy.setDirection(DcMotorSimple.Direction.REVERSE);
+        }
+        servoGrippy.setPower(gripperPower);
+    }
+
+    //PID FOR MOVING THE CRANE
+    public double PIDControlCrane(double craneTarget){
+
+        double cranePos = motorCrane1.getCurrentPosition();
+        double craneError = craneTarget - cranePos;
+        craneIntegralSum += craneError * craneTimer.seconds();
+        double craneDerivative = (craneError - craneLastError) / craneTimer.seconds();
+        craneLastError = craneError;
+
+        craneTimer.reset();
+
+        double craneOutput = (craneError * craneP) + (craneDerivative * craneD) + (craneIntegralSum * craneI);
+        return craneOutput;
+    }
+
+    //RETURNS THE POWER CALCULATED BY THE PID
+    public double cranePower(double craneTarget){
+        double cranePos = motorCrane1.getCurrentPosition();
+        double craneFF = Math.cos(Math.toRadians((cranePos - craneOffset) / threetwelvemotorticksindegree)) * craneF;
+        double cranePower = PIDControlCrane(craneTarget) + craneFF;
+        return cranePower;
+    }
+
+
+
+}
--- readme.md
+++ readme.md
@@ -2,6 +2,25 @@
 
 Acesta este un repository unde se vor posta temele pentru bobocii din 2024.
 
+## Ce îți trebuie pentru a doua temă?
+
+Tot ce ai nevoie este un IDE ca Android Studio sau un editor de text, preferabil unul care are sintaxă de evidențiere, cum ar fi Visual Studio Code sau Notepad++.
+
+## Care este tema?
+
+Pentru aceasta tema trebuie sa te uiti in clase si sa modifici urmatoarele lucruri:
+-Sa adaugi tolerante mai mari pentru triggerele de la controller de la 0.1 la 0.2
+-Sa schimbi directia slideurilor de la crane astfel incat right si left bumper au functiile inversate
+-Sa schimbi CraneOffset de la 90 la 100 de grade
+
+## Cum îmi trimit tema?
+
+Trimiterea temei este, de fapt, parte din temă. Aceasta constă în crearea unui fork pe GitHub.com, unde vei posta codul tău modificat. Butonul de fork se găsește în [repo](https://github.com/robosapiensprogramare/Boboci2024), în dreapta sus a paginii. De acolo, trebuie să reușești să pui codul modificat în repository-ul tău, fie prin GitHub.com, fie prin orice alt IDE sau editor de text cu integrare pentru Git.
+
+## Dacă nu știu cum să fac ceva din temă?
+
+Dacă întâmpini o problemă în procesul de rezolvare a temei, te încurajăm să cauți resurse online specifice problemei tale, atâta timp cât înțelegi cum s-a rezolvat. De asemenea, poți trimite un mesaj pe grupul de programare pentru boboci, unde un membru al echipei te va ajuta.
+
 ## Cum descarci acest repository?
 
 ### 1. De pe GitHub.com
@@ -13,19 +32,3 @@ Accesați [repoul](https://github.com/robosapiensprogramare/Boboci2024). Deasupr
 ```bash
 git clone https://github.com/robosapiensprogramare/Boboci2024
 ```
-
-## Ce îți trebuie pentru prima temă?
-
-În prezent, tot ce ai nevoie este un editor de text, preferabil unul care are sintaxă de evidențiere, cum ar fi Visual Studio Code sau Notepad++, sau un IDE ca Android Studio (important pentru temele viitoare) sau Microsoft Visual Studio.
-
-## Care este tema?
-
-Primul lucru pe care trebuie să-l faci este să răsfoiești prin fișierele din repository și să adaugi comentarii la tot ce înțelegeti. Sintaxa pentru comentarii este `//` înainte de textul pe care vrei să-l adaugi.
-
-## Cum îmi trimit tema?
-
-Trimiterea temei este, de fapt, parte din temă. Aceasta constă în crearea unui fork pe GitHub.com, unde vei posta codul tău modificat. Butonul de fork se găsește în [repo](https://github.com/robosapiensprogramare/Boboci2024), în dreapta sus a paginii. De acolo, trebuie să reușești să pui codul modificat în repository-ul tău, fie prin GitHub.com, fie prin orice alt IDE sau editor de text cu integrare pentru Git.
-
-## Dacă nu știu cum să fac ceva din temă?
-
-Dacă întâmpini o problemă în procesul de rezolvare a temei, te încurajăm să cauți resurse online specifice problemei tale, atâta timp cât înțelegi cum s-a rezolvat. De asemenea, poți trimite un mesaj pe grupul de programare pentru boboci, unde un membru al echipei te va ajuta.
